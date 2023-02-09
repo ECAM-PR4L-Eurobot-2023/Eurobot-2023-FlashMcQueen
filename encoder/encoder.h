65c324302b63ae320f487cb7a27a50ea57b0f185
Alejandro <alejandrobrb210501@gmail.com>
2023-02-09 20:41:48 +0100

    (#5) Implement the module that manage a quadratic encoder  * Able to create a single instance for an encoder * Used only for quadratic encoders

---
+++ encoder/encoder.h
@@ -0,0 +1,31 @@
+#ifndef ENCODER_H
+#define ENCODER_H
+
+#include <Arduino.h>
+
+typedef long long int counter_unit;
+
+typedef struct {
+    const gpio_num_t pin;
+} SingleEncoder;
+
+class Encoder {
+  public:
+    const SingleEncoder encoder_a;
+    const SingleEncoder encoder_b;
+    volatile counter_unit counter;
+
+public:
+    // Constructor
+    Encoder(int, int);
+
+    void begin(void);
+    void reset_counter(void);
+    counter_unit get_counter(void);
+    void increment_counter(void);
+    void decrement_counter(void);
+    const SingleEncoder *get_encoder_a(void);
+    const SingleEncoder *get_encoder_b(void);
+};
+
+#endif /* ENCODER_H */
