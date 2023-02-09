20fcb7d3e94f62de333d37729b8261a547088337
Alejandro <alejandrobrb210501@gmail.com>
2023-02-09 21:21:30 +0100

    (#5) Put the encoder.h and encoder.cpp files in /src/driver

--- encoder/encoder.ino
+++ encoder/encoder.ino
@@ -1,22 +1,22 @@
 #include <Arduino.h>
-
-#include "encoder.h"
 #include <driver/gpio.h>
+
+#include "src/driver/encoder.h"
 
 Encoder encoder_left(25, 26);
 Encoder encoder_right(33, 32);
 
 void setup() {
 	Serial.begin(115200);
-  encoder_left.begin();
-  encoder_right.begin();
+    encoder_left.begin();
+    encoder_right.begin();
 }
 
 void loop() {
-  Serial.print("Encoder left: ");
-	Serial.println(encoder_left.get_counter());
-  Serial.print("Encoder right: ");
-	Serial.println(encoder_right.get_counter());
+    Serial.print("Encoder left: ");
+    Serial.println(encoder_left.get_counter());
+    Serial.print("Encoder right: ");
+    Serial.println(encoder_right.get_counter());
 
   delay(100);
 }
