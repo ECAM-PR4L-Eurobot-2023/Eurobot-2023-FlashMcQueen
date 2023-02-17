a4f11122ec81234e47cbf0e54bd5b142e8cc7725
olingo99 <engels.theo@skynet.be>
2023-02-17 16:17:56 +0100

    oui

--- encoder/encoder.ino
+++ encoder/encoder.ino
@@ -15,20 +15,26 @@ Locator locator(&encoder_left, &encoder_right);
 
 Position position{0, 0, 0, 0};
 
-Moteur mot1 = Moteur(26, 27, 14);
-Moteur mot2 = Moteur(25, 33, 32);
+Moteur moteurR(26,27,14);
+Moteur moteurL(33,25,32);
+
 
 
 double in1 = 0;
 double out1 = 0;
-double set1 = 0;
+double set1 = 350;
 
 double in2 = 0;
 double out2 = 0;
-double set2 = 0;
+double set2 = 350;
+
+PID p1(&in1, &out1, &set1, 0.30, 0.08, 0, -255, 255, 400);
+PID p2(&in2, &out2, &set2, 0.30, 0.08, 0, -255, 255, 400);
 
 void setup() {
 	Serial.begin(115200);
+  moteurL.begin();
+  moteurR.begin();
   locator.begin();
 }
 
@@ -61,14 +67,28 @@ void loop() {
   // Serial.println(position.x);
   // Serial.print("y: ");
   // Serial.println(position.y);
-  in1 = getDistRun(locator.get_position(), position);
-  in2 = getDistRun(locator.get_position(), position);
+  // in1 = getDistRun(locator.get_position(), position);
+  // in2 = getDistRun(locator.get_position(), position);
+  in1 = encoder_left.get_speed_mm_s()/10;
+  in2 = encoder_right.get_speed_mm_s()/10;
   p1.compute();
   p2.compute();
+  moteurL.setTension(out1);
+  moteurR.setTension(out2);
+  Serial.print("in1: ");
+  Serial.println(in1);
+  Serial.print("out1: ");
+  Serial.println(out1);
+  Serial.print("in2: ");
+  Serial.println(in2);
+  Serial.print("out2: ");
+  Serial.println(out2);
+
+  // moteur1.setTension(100);
+  // moteur2.setTension(100);
 
 
-
-  delay(100);
+  delay(200);
 }
 
 double getDistRun(Position start, Position end){
